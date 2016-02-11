a54537b7cb6deb3f675712749f75df0c2d1c6c5a
BrunoDM2943 <bdm2943@gmail.com>
2016-02-11 11:27:10 -0200

    Signed-off-by: BrunoDM2943 <bdm2943@gmail.com>

--- src/main/java/com/bitshammer/autor/bean/AutorBean.java
+++ src/main/java/com/bitshammer/autor/bean/AutorBean.java
@@ -3,12 +3,53 @@
  */
 package com.bitshammer.autor.bean;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.bitshammer.autor.facade.IAutorFacade;
 import com.bitshammer.infra.bean.DefaultBean;
+import com.bitshammer.model.Autor;
 
 /**
  * @author bruno.martins
  *
  */
+@Named
+@RequestScoped
 public class AutorBean extends DefaultBean {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private IAutorFacade facade;
+	
+	private Autor autor;
+	
+	public AutorBean() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void salvar(){
+		facade.save(autor);
+	}
+
+	/**
+	 * @return the autor
+	 */
+	public Autor getAutor() {
+		return autor;
+	}
+
+	/**
+	 * @param autor the autor to set
+	 */
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+	
 
 }
