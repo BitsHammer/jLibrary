81e03b15870393a3798d142d9eb24bf2c8f41580
BrunoDM2943 <bdm2943@gmail.com>
2016-03-25 12:22:18 -0300

    Tomcat embarcado

--- src/main/java/com/bitshammer/launch/Main.java
+++ src/main/java/com/bitshammer/launch/Main.java
@@ -2,7 +2,11 @@ package com.bitshammer.launch;
 
 import java.io.File;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
 
 public class Main {
 
@@ -21,10 +25,19 @@ public class Main {
 
 		tomcat.setPort(Integer.valueOf(webPort));
 
-		tomcat.addWebapp("/", new File(
+		Context context = tomcat.addWebapp("/", new File(
 				webappDirLocation).getAbsolutePath());
 		System.out.println("configuring app with basedir: "
 				+ new File("./" + webappDirLocation).getAbsolutePath());
+		
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(context);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        context.setResources(resources);
+
 
 		tomcat.start();
 		tomcat.getServer().await();
