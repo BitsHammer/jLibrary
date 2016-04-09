4b03350f9d41752f78439416757c65b09c5f9421
BrunoDM2943 <bdm2943@gmail.com>
2016-04-09 14:44:33 -0300

    Atualização de cliente
    Signed-off-by: BrunoDM2943 <bdm2943@gmail.com>

--- src/test/java/com/bitshammer/test/cliente/ClienteTest.java
+++ src/test/java/com/bitshammer/test/cliente/ClienteTest.java
@@ -92,4 +92,39 @@ public class ClienteTest {
 		facade.cadastrarCliente(cliente);
 		facade.descadastrarCliente(cliente);
 	}
+	
+	@Test
+	public void atualizarCliente(){
+		Cliente cliente = new Cliente();
+		cliente.setNome("Bruno");
+		cliente.setCpf("43598392869");
+		cliente.setDtNascimento(new Calendar.Builder().setDate(1995, 05, 10).build().getTime());
+		cliente.setCelular("953200587");
+		cliente.setTelefone("29435002");
+		cliente.setSexo(Sexo.F);
+		
+		//Endereco
+		Endereco endereco = new Endereco();
+		endereco.setBairro("Parque Boturussu");
+		endereco.setCep("03805090");
+		endereco.setLocalidade("São Paulo");
+		endereco.setComplemento("Proximo a avendia Paranagua");
+		endereco.setUf("SP");
+		endereco.setNumero(661);
+		
+		//Usuario
+		Usuario usuario = new Usuario();
+		usuario.setEmail("bdm2943@gmail.com");
+		usuario.setLogin("bruno");
+		usuario.setSenha("12345a");
+		usuario.setTipoUsuario(TipoUsuario.CLIENTE);
+		
+		cliente.setEndereco(endereco);
+		cliente.setUsuario(usuario);
+		
+		facade.cadastrarCliente(cliente);
+		facade.atualizarCliente(cliente);
+		
+	}
+	
 }
